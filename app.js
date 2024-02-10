bf5c211fad5b06b01f570fb9363a28fcf27ad986
crd47 <jordrichh7@gmail.com>
2024-02-10 17:15:51 +0300

    the base files

---
+++ app.js
@@ -0,0 +1,20 @@
+const express=require('express')
+const path= require('path')
+const app=express()
+const mongoose=require('mongoose')
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'))
+
+const dbUri="mongodb://localhost:27017/mydb/users";
+mongoose.connect(dbUri,{useNewUrlParser:true, useUnifiedTopology: true })
+.then((result)=>app.listen(5000))
+.catch((error)=>{
+    console.error(error)
+})
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+app.get('/cars',(req,res)=>{
+      res.render('cars')
+})
