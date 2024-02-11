0c478c4c8d086624c5b24256e5eea8957f098528
crd47 <jordrichh7@gmail.com>
2024-02-11 12:19:06 +0300

    introduced post hook to the database

--- model/user.js
+++ model/user.js
@@ -16,5 +16,11 @@ const UserSchema=new mongoose.Schema({
         lowecase:[true,"email has to be in lowercase"]
     }
 })
+//post is a hook used to create a process after something has been done
+UserSchema.post('save',(doc,next)=>{
+    console.log("new user has been created and saved",doc)
+    next()
+})
 const User=mongoose.model('users',UserSchema)
+
 module.exports=User
